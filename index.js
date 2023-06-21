af80e4954e5fc849e36777762a6ce00cedc640af
Vivek Patel <vpatel360@gmail.com>
2023-06-20 23:29:18 -0400

    commit tree b643ebfc87e653b2ddf84e723355bc8be4f18cb3

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
+const cats = ["Milo", "Otis", "Garfield"]
 
-const cats = ["Milo", "Otis", "Garfield"];
-
-function destructivelyAppendCat(name) {
-    cats.push("Ralph");
-    return cats;
-};
-
-function destructivelyPrependCat(name) {
-    cats.unshift("Bob");
-    return cats;
-};
-
-function destructivelyRemoveLastCat(name) {
-    cats.pop();
-    return cats;
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
 }
 
-function destructivelyRemoveFirstCat(name) {
-    cats.shift();
-    return cats;
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
 }
 
-function appendCat(name) {
-    const newCats = [...cats, "Broom"];
-    return newCats;
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
 }
 
-function prependCat(name) {
-    const newCats = ["Arnold", ...cats];
-    return newCats;
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
 }
 
-function removeLastCat(name) {
-    return cats.slice(0, -1);
+function appendCat(name){
+    const newCats = [...cats, name]
+    return newCats
 }
 
-function removeFirstCat(name) {
-    return cats.slice(1);
+function prependCat(name){
+    const newCats = [name, ...cats]
+    return  newCats
+}
+
+function removeLastCat(){
+    return cats.slice(0,2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
 }
